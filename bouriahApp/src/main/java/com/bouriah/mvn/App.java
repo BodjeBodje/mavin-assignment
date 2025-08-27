c2f69073fb9fce3f3f6b3c30ee8296a8d86515e4
BodjeBodje <bouriahdiallo93@gmail.com>
2025-08-27 14:15:46 +0000

    mavin assignment and .github>workflow>build.yml

---
+++ bouriahApp/src/main/java/com/bouriah/mvn/App.java
@@ -0,0 +1,10 @@
+package com.bouriah.mvn;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World class 24 assignment !");
+    }
+}
